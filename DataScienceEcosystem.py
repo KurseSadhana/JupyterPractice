4c601c09faebcfd8e75d7ef2500e4a75b0110601
KurseSadhana <48557584+KurseSadhana@users.noreply.github.com>
2024-01-28 20:20:36 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Data Science Tools and Ecosystem</h1>
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# <b>Objectives:</b>
+# <ul>
+#     <li> 5 Languages that Data scientists use </li>
+#     <li> Libraris that Data Scientists use</li>
+#     <li> Data science tools </li>
+#     <li> Expression in Jypiter </li>
+#    </ul>
+
+# Some of the popular languages that Data Scientists use are:
+#  1. Python
+#  2. R
+#  3. Java 
+#  4. Matlab
+#  5. SQL
+#  6. Scala
+
+# Some of the commonly used libraries used by Data Scientists include
+# <ul><li>NumPy</li>
+# <li>Pandas</li>
+# <li>Matplotlib</li>
+# <li>Seaborn</li>
+# <li>NLTK (Natural Language Toolkit)</li><ul>
+
+# <table>
+# <tr>
+# <th>Data Science Tools</th>
+# </tr>
+# 
+# <tr>
+# <td>Python</td>
+# </tr>
+# 
+# <tr>
+# <td>R</td>
+# </tr>
+#     
+# <tr>
+# <td>Jupyter notebooks</td>
+# </tr>
+# </table>
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python</h3>
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[10]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[11]:
+
+
+200/60
+
+
+# <h2>Author</h2>
+# Sadhana
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
