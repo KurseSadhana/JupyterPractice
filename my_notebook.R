5015f736b7ff2e18753c078d9ae89b65c5dd2cec
KurseSadhana <48557584+KurseSadhana@users.noreply.github.com>
2024-01-28 20:20:05 +0530

    Delete my_notebook.R

--- my_notebook.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-autompg = read.table(
-  "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data",
-  quote = "\"",
-  comment.char = "",
-  stringsAsFactors = FALSE)
-head(autompg,20)
